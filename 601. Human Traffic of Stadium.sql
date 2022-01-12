2ab92365e2f6a592dfb68abbff02d1c0157c86ea
roy1337 <40725093+roy1337@users.noreply.github.com>
2022-01-12 08:44:11 +0200

    Update 601. Human Traffic of Stadium.sql

--- 601. Human Traffic of Stadium.sql
+++ 601. Human Traffic of Stadium.sql
@@ -1,4 +1,4 @@
-Write an SQL query to display the records with three or more rows with consecutive id's, and the number of people is greater than or equal to 100 for each.
+Write an SQL query to display the records with three or more rows with consecutive ids, and the number of people is greater than or equal to 100 for each.
 
 Return the result table ordered by visit_date in ascending order.
 

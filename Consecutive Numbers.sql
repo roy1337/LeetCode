40a082203fea19900f89c8643fc08869cb86b452
Roy <40725093+roy1337@users.noreply.github.com>
2022-01-13 14:58:21 +0200

    Update Consecutive Numbers.sql

--- Consecutive Numbers.sql
+++ Consecutive Numbers.sql
@@ -9,3 +9,18 @@ select distinct l1.num as ConsecutiveNums
         on l2.id = l3.id -1
         where l1.num = l2.num
         and l2.num = l3.num
+
+
+
+one more answer 
+
+ with cte as (
+ select 
+ lead(num,1) over () as oneafter,
+ lag(num,1) over () as onebefore,
+ num
+ from logs
+)
+
+select distinct num as ConsecutiveNums from cte
+where num=oneafter and num=onebefore

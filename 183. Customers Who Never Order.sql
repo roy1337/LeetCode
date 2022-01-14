6d7cc42b3d1a601f53ec4859479d4b12034e5a99
Roy <40725093+roy1337@users.noreply.github.com>
2022-01-14 13:01:37 +0200

    Update 183. Customers Who Never Order.sql

--- 183. Customers Who Never Order.sql
+++ 183. Customers Who Never Order.sql
@@ -1,9 +1,5 @@
 183. Customers Who Never Order
 Write an SQL query to report all customers who never order anything.
 
-Return the result table in any order.
-
-The query result format is in the following example.
-
 select name as Customers from customers cc
 where cc.id not in (select customerid from orders)

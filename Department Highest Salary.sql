f8c6198f2ef31037aa8ee4f41f7246e0502ed2bb
Roy <40725093+roy1337@users.noreply.github.com>
2022-01-16 10:18:37 +0200

    Update Department Highest Salary.sql

--- Department Highest Salary.sql
+++ Department Highest Salary.sql
@@ -4,6 +4,24 @@ Return the result table in any order.
 
 The query result format is in the following example.
 
+
+
+with cte as (
+select dd.name as dename,ee.name as ename,salary, rank() over (partition by dd.name order by salary desc) as ranked
+from employee ee
+join department dd
+on ee.departmentid = dd.id
+)
+
+select dename as Department , ename as Employee ,Salary
+from cte
+where ranked = 1
+
+
+
+
+Second Solution
+
 with cte as (
 select  de.name as dename, max(ee.salary) as maxsal
 from employee ee
